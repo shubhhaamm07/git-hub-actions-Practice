5a3d0221d17f94d904fa9dd5fedbcc1e114b7005
Shubham Rana <shubhamrana009988@gmail.com>
2026-09-20 00:45:58 +0530

    Fix formatting and add main execution block

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-#Hello a
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -11,3 +11,7 @@ def hello_world():
 @app.route('/health')
 def health():
     return 'Server is up and running'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
